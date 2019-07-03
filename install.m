c46c20e045c83aaca6559e35d59e690bb6f1d912
Pavel Roslovets <roslovets@exponenta.ru>
2019-07-03 21:15:12 +0300

    Toolbox binary is excluded from source files

--- install.m
+++ install.m
@@ -1,4 +1,4 @@
 % Generated with Toolbox Extender https://github.com/ETMC-Exponenta/ToolboxExtender
-matlab.addons.install('Icons.mltbx');
-disp('Icons has been installed');
+dev = IconsDev;
+dev.test;
 % Add post-install commands below
